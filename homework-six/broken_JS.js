edb9b81e600e7d81c6a9cebb5642df20be785448
dinhanh0 <anhcongdinh0711@gmail.com>
2025-08-04 11:53:41 -0400

    Add files via upload

---
+++ homework-six/broken_JS.js
@@ -0,0 +1,51 @@
+let PlayerChoice = document.getElementsByClassName("choice");
+
+let CompArray = ["Rock", "Paper", "Scissors"];
+let innerscore = 0;
+
+function RPS(numb) {
+  let CompDecision = CompArray[Math.floor(Math.random() * CompArray.length)];
+  
+  document.getElementById("resultDecision").innerHTML = "The computer chose:" + CompDecision;
+  
+  //NOTE: If a tie, subtract -0.5
+
+  if (numb == 0) {
+    if (CompDecision == "Paper") {
+      innerscore -= 1;
+    }
+    else if (CompDecision == "Scissors") {
+      innerscore += 1;
+    }
+    else {
+      innerscore -= 0.5;
+    }
+
+  }
+
+  if (numb == 1) {
+    if (CompDecision == "Rock") {
+      innerscore += 1;
+    }
+    else if (CompDecision == "Scissors") {
+      innerscore -= 1;
+    }
+    else {
+      innerscore -= 0.5;
+    }
+  }
+
+  if (numb == 2) {
+    if (CompDecision == "Rock") {
+      innerscore -= 1;
+    }
+    else if (CompDecision == "Paper") {
+      innerscore += 1;
+  }
+    else {
+      innerscore -= 0.5;
+  }
+}
+
+  document.getElementById("Score").innerHTML = "Score: " + innerscore;
+}
